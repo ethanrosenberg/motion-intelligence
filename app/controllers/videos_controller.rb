cf9f826bb40f0d6f017b8f61185796c9c43d1aa0
Ethan Rosenberg <ethan@ethanrosenberg.com>
2020-02-17 23:30:19 -0800

    Using test bug to help figure out SocketError.

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -6,21 +6,32 @@ class VideosController < ApplicationController
   def index
 
 
-
-    # Authenticate with your API key
-    apiKey = ENV['ALGORITHMIA_API']
+    input = "ethanrosenberg"
+    client = Algorithmia.client('simT1luO+gWfSZxNgCEslhTYg8A1')
+    algo = client.algo('ethanrosenberg/Hello/0.1.0')
+    algo.set('timeout':300) # optional
+    puts algo.pipe(input).result
 
     byebug
-    # Create the Algorithmia client object
-    client = Algorithmia.client(apiKey)
+    # Authenticate with your API key
+    #apiKey = ENV['ALGORITHMIA_API']
+
 
     input = {
       video: "https://www.youtube.com/watch?v=OMgIPnCnlbQ",
       detector: "content"
     }
+    client = Algorithmia.client('simT1luO+gWfSZxNgCEslhTYg8A1')
     algo = client.algo('media/SceneDetection/0.1.6')
     algo.set('timeout':300) # optional
     puts algo.pipe(input).result
+
+
+
+    #algo = client.algo('media/SceneDetection/0.1.6')
+    #algo.set('timeout':300) # optional
+
+    #puts algo.pipe(input).result
 
 
 
