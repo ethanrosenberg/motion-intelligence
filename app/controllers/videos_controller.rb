f17aeaac2b914ddfcd2f4ae55142450a87336de8
Ethan Rosenberg <ethan@ethanrosenberg.com>
2020-02-20 23:49:50 -0800

    Added API_KEY as new const to VideosController.

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -3,6 +3,7 @@ require 'algorithmia'
 
 class VideosController < ApplicationController
 
+ API_KEY = 'simT1luO+gWfSZxNgCEslhTYg8A1'
 
   def process_video(url)
 
@@ -14,14 +15,14 @@ class VideosController < ApplicationController
       output_collection_frames: "scenegrabs"
     }
 
-    api_key = 'simT1luO+gWfSZxNgCEslhTYg8A1'
+    #api_key = 'simT1luO+gWfSZxNgCEslhTYg8A1'
 
     puts 'Processing video: ' + url + ' || timeout: 20 min'
 
     response = HTTParty.post("https://api.algorithmia.com/v1/algo/media/SceneDetection/0.1.6",
               {
                 :body => input.to_json,
-                :headers => { 'Content-Type' => 'application/json', 'Accept' => 'application/json', 'Authorization' => api_key},
+                :headers => { 'Content-Type' => 'application/json', 'Accept' => 'application/json', 'Authorization' => API_KEY},
                 :timeout => 1200
               })
 
@@ -35,11 +36,9 @@ class VideosController < ApplicationController
   end
 
   def normalize_frame_urls(frames)
-
     #frames = ["data://.my/scenegrabs/00:00:05.640.png", "data://.my/scenegrabs/00:00:07.320.png", "data://.my/scenegrabs/00:00:09.760.png", "data://.my/scenegrabs/00:00:11.840.png", "data://.my/scenegrabs/00:00:15.800.png", "data://.my/scenegrabs/00:00:18.960.png", "data://.my/scenegrabs/00:00:21.640.png", "data://.my/scenegrabs/00:00:27.240.png", "data://.my/scenegrabs/00:00:30.520.png"]
     cleaned_frames = frames.map {|frame| frame.gsub('data://.my/scenegrabs/', 'https://algorithmia.com/v1/data/ethanrosenberg/scenegrabs/') }
     return cleaned_frames
-
   end
 
   def index
